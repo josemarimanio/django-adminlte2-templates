d0dc3ee56c6d8fe2b5189846497ca8e15f97f3a3
josemarimanio <josemari.manio@outlook.com>
2020-03-22 16:14:17 +0800

    Initial version of module constants

---
+++ adminlte2_templates/constants.py
@@ -0,0 +1,47 @@
+# CDN links for AdminLTE 2.4.18
+ADMINLTE_CDN_ADMINLTE_CSS_CORE = 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.18/css/AdminLTE.min.css'
+ADMINLTE_CDN_ADMINLTE_CSS_SKIN = {
+    'skin-black': 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.18/css/skins/skin-black.min.css',
+    'skin-black-light': 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.18/css/skins/skin-black-light.min.css',
+    'skin-blue': 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.18/css/skins/skin-blue.min.css',
+    'skin-blue-light': 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.18/css/skins/skin-blue-light.min.css',
+    'skin-green': 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.18/css/skins/skin-green.min.css',
+    'skin-green-light': 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.18/css/skins/skin-green-light.min.css',
+    'skin-purple': 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.18/css/skins/skin-purple.min.css',
+    'skin-purple-light': 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.18/css/skins/skin-purple-light.min.css',
+    'skin-red': 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.18/css/skins/skin-red.min.css',
+    'skin-red-light': 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.18/css/skins/skin-red-light.min.css',
+    'skin-yellow': 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.18/css/skins/skin-yellow.min.css',
+    'skin-yellow-light': 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.18/css/skins/skin-yellow-light.min.css',
+}
+ADMINLTE_CDN_ADMINLTE_JS_CORE = 'https://cdnjs.cloudflare.com/ajax/libs/admin-lte/2.4.18/js/adminlte.min.js'
+
+# CDN links for Bootstrap 3.4.1
+ADMINLTE_CDN_BOOTSTRAP_CSS_CORE = 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/css/bootstrap.min.css'
+ADMINLTE_CDN_BOOTSTRAP_JS_CORE = 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.4.1/js/bootstrap.min.js'
+
+# CDN link for Font-Awesome 4.7.0
+ADMINLTE_CDN_FONTAWESOME_CSS_CORE = 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'
+
+# CDN link for jQuery 3.4.1
+ADMINLTE_CDN_JQUERY_JS_CORE = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js'
+
+# Valid AdminLTE skin style values
+SKIN_STYLE_BLACK = 'skin-black'
+SKIN_STYLE_BLACK_LIGHT = 'skin-black-light'
+SKIN_STYLE_BLUE = 'skin-blue'
+SKIN_STYLE_BLUE_LIGHT = 'skin-blue-light'
+SKIN_STYLE_GREEN = 'skin-green'
+SKIN_STYLE_GREEN_LIGHT = 'skin-green-light'
+SKIN_STYLE_PURPLE = 'skin-purple'
+SKIN_STYLE_PURPLE_LIGHT = 'skin-purple-light'
+SKIN_STYLE_RED = 'skin-red'
+SKIN_STYLE_RED_LIGHT = 'skin-red-light'
+SKIN_STYLE_YELLOW = 'skin-yellow'
+SKIN_STYLE_YELLOW_LIGHT = 'skin-yellow-light'
+SKIN_STYLE_CHOICES = (SKIN_STYLE_BLACK, SKIN_STYLE_BLACK_LIGHT, SKIN_STYLE_BLUE, SKIN_STYLE_BLUE_LIGHT,
+                      SKIN_STYLE_GREEN, SKIN_STYLE_GREEN_LIGHT, SKIN_STYLE_PURPLE, SKIN_STYLE_PURPLE_LIGHT,
+                      SKIN_STYLE_RED, SKIN_STYLE_RED_LIGHT, SKIN_STYLE_YELLOW, SKIN_STYLE_YELLOW_LIGHT)
+
+# Default AdminLTE skin style
+DEFAULT_SKIN_STYLE = SKIN_STYLE_BLUE
