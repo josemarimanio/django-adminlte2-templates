979f8cd25f67786260fc22b729fc2f9181002d4a
josemarimanio <josemari.manio@outlook.com>
2020-03-22 16:14:39 +0800

    Updated context processor, added variables for CDN

--- adminlte2_templates/context_processors.py
+++ adminlte2_templates/context_processors.py
@@ -1,19 +1,50 @@
 from django.conf import settings
+from adminlte2_templates import constants
+
+
+def _get_context(context):
+    """
+        Return the context from Django settings.
+        If no context is found, get default value from module constants
+    """
+    return getattr(settings, context, getattr(constants, context))
 
 
 def template(request):
     """
-        Get all settings related to the AdminLTE 2 module and
-        return them as context variables
+        Get all settings related to the AdminLTE 2 module and return them as context variables
     """
-    context = {}
+    skin_style = getattr(settings, 'ADMINLTE_SKIN_STYLE', constants.DEFAULT_SKIN_STYLE)
 
-    # Skin style color. Valid values are:
-    #   skin-black, skin-black-light, skin-blue, skin-blue-light, skin-green, skin-green-light, skin-purple,
-    #   skin-purple-light, skin-red, skin-red-light, skin-yellow, skin-yellow-light
-    try:
-        context['ADMINLTE_SKIN_STYLE'] = settings.ADMINLTE_SKIN_STYLE
-    except AttributeError:
-        context['ADMINLTE_SKIN_STYLE'] = 'skin-blue'
+    context = {
+        'DEBUG': getattr(settings, 'DEBUG'),
+
+        #
+        # Skin style color. Valid values are:
+        #   skin-black, skin-black-light, skin-blue, skin-blue-light, skin-green, skin-green-light, skin-purple,
+        #   skin-purple-light, skin-red, skin-red-light, skin-yellow, skin-yellow-light
+        #
+        'ADMINLTE_SKIN_STYLE': skin_style,
+
+        #
+        # Toggle to use CDN for AdminLTE dependencies
+        #
+        'ADMINLTE_USE_CDN': getattr(settings, 'ADMINLTE_USE_CDN', False),
+
+        #
+        # Dependency CDN URLs
+        #
+        # AdminLTE 2.4.18
+        'ADMINLTE_CDN_ADMINLTE_CSS_CORE': _get_context('ADMINLTE_CDN_ADMINLTE_CSS_CORE'),
+        'ADMINLTE_CDN_ADMINLTE_CSS_SKIN': _get_context('ADMINLTE_CDN_ADMINLTE_CSS_SKIN')[skin_style],
+        'ADMINLTE_CDN_ADMINLTE_JS_CORE': _get_context('ADMINLTE_CDN_ADMINLTE_JS_CORE'),
+        # Bootstrap 3.4.1
+        'ADMINLTE_CDN_BOOTSTRAP_CSS_CORE': _get_context('ADMINLTE_CDN_BOOTSTRAP_CSS_CORE'),
+        'ADMINLTE_CDN_BOOTSTRAP_JS_CORE': _get_context('ADMINLTE_CDN_BOOTSTRAP_JS_CORE'),
+        # Font-Awesome 4.7.0
+        'ADMINLTE_CDN_FONTAWESOME_CSS_CORE': _get_context('ADMINLTE_CDN_FONTAWESOME_CSS_CORE'),
+        # jQuery 3.4.1
+        'ADMINLTE_CDN_JQUERY_JS_CORE': _get_context('ADMINLTE_CDN_JQUERY_JS_CORE')
+    }
 
     return context
