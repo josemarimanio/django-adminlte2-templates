3e0ac1920d5f0b52789ef3dc6f6541ac1cd496c7
josemarimanio <josemari.manio@outlook.com>
2020-09-17 02:22:30 +0800

    Updated example project "example_project"

--- example_project/example_project/urls.py
+++ example_project/example_project/urls.py
@@ -1,8 +1,10 @@
+from django.contrib import admin
 from django.urls import include
 from django.urls import path
 from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('layouts/', include('layouts.urls')),
 
     path('', RedirectView.as_view(pattern_name='layouts:index'), name='index'),
